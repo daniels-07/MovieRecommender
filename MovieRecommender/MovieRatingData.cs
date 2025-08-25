dda9ed83d7f1b9b3670ae4dcf77e9ea431b834af
30069589 <30069589@TH-8G25-604409>
2025-08-25 11:41:57 +0800

    Add project files.

---
+++ MovieRecommender/MovieRatingData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRecommender
+{
+    internal class MovieRatingData
+    {
+        public class MovieRating
+        {
+            [LoadColumn(0)]
+                public float userId;
+            [LoadColumn(1)]
+                public float movieId;
+            [LoadColumn(2)]
+            public float Label;
+        }
+        public class MovieRatingPrediction
+        {
+            public float Lavel;
+            public float Score;
+        }
+    }
+}
---
+++ MovieRecommender/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace MovieRecommender
+{
+    using Microsoft.ML;
+    using Microsoft.ML.Trainers;
+    using MovieRecommendation;
+    using System.Data;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+        }
+        private static void LoadData(MLContext mLContext)
+        {
+            (IDataView training, IDataView test) LoadData(MLContext mlContext)
+            {
+                ITransformer model = BuildAndTrainModel(mlContext, trainingDataView);
+                var trainingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+                var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
+
+                IDataView trainingDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+                IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
+
+                (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+                ITransformer model = BuildAndTrainModel(mlContext, trainingDataView);
+
+                return (trainingDataView, testDataView);
+            }
+        }
+        ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainingDataView)
+        {
+            IEstimator<ITransformer> estimator = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+                .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "movieIdEncoded", inputColumnName: "movieId"));
+
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "movieIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 20,
+                ApproximationRank = 100
+            };
+
+            var trainerEstimator = estimator.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer model = trainerEstimator.Fit(trainingDataView);
+
+            return model;
+        }
+        void EvaluateModel(MLContext mlContext, IDataView testDataView, ITransformer model)
+        {
+            Console.WriteLine("=============== Evaluating the model ===============");
+            var prediction = model.Transform(testDataView);
+
+            var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+
+            Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
+
+            EvaluateModel(mlContext, testDataView, model);
+        }
+    }
+}
